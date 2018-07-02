302778dce4ab35675f586eab2a771acc49af4dc5
hkey <mickey.20@qq.com>
2018-07-02 22:28:49 +0800

    day14

--- day14/s2.py
+++ day14/s2.py
@@ -1,2 +1,34 @@
 # -*- coding: utf-8 -*-
 # Author: hkey
+
+# list1 = ['a', 'b', 'c']
+# print(id(list1))
+# list1.insert(3, 'one')
+# print(list1)
+# print(id(list1))
+# list1 = ['a', 'b', 'c']
+# print(id(list1))
+
+# a = 'hello xiaofei'
+#
+# b = a
+# print(id(a))
+# print(id(b))
+#
+# a = 'hello hkey'
+# print(a)
+# print(b)
+
+list1 = [1, 2, 3, 4, [5, 6]]
+
+list2 = list1.copy()
+print(id(list1))
+print(id(list2))
+
+list2.pop(0)
+print(list1)
+print(list2)
+
+list2[-1][-1] = 88888
+print(list1)
+print(list2)
