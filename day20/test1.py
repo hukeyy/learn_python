2a1607c94b515146ba35b35299cf876dc87fa81c
hkey <mickey.20@qq.com>
2018-07-23 08:24:48 +0800

    day18-计算器

---
+++ day20/test1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Author: hkey
+
+class Brith:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+class Teacher:
+    def __init__(self, name, course):
+        self.name = name
+        self.course = course
+        self.brith = Brith(1990, 10, 23)
+
+
+
+
+
