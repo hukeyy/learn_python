5818629fcc21ac5f2f0606c5935b62efd27f2d31
hkey <mickey.20@qq.com>
2018-08-27 10:18:30 +0800

    day24-选课系统

--- day24-选课系统/core/school_center.py
+++ day24-选课系统/core/school_center.py
@@ -29,19 +29,25 @@ class Manage_school(object):
             for key in self.school_db:
                 key.cat_school()
                 self.db_main[key.name] = key
-            choice_school = input('\033[34;1m请选择学校名：\033[0m').strip()
-            if not choice_school: continue
-            if choice_school in self.db_main:
-                print('\033[33;1m欢迎进入【%s】学校\033[0m'.center(50, '#') % choice_school)
-                self.school = self.db_main[choice_school]
+            school_name = input('\033[34;1m请选择学校名：\033[0m').strip()
+            if not school_name: continue
+            if school_name in self.db_main:
+                print('\033[33;1m欢迎进入【%s】学校\033[0m'.center(50, '#') % school_name)
+                self.school = self.db_main[school_name]
                 while flag:
                     choice = options(settings.school_list)
                     # school_list = ['创建课程', '招聘讲师', '创建班级', '退出']
                     if choice == '1':
                         print('\033[32;1m创建课程\033[0m')
                         self.course_db = self.school_db[self.school]
-
+                        if self.course_db:
+                            print('\033[33;1m目前【%s】已有课程\033[0m'.center(50, '#') % school_name)
+                            for key in self.course_db:
+                                key.cat_course()
+                        else:
+                            print('\033[32;1m目前【%s】学校没有课程.\033[0m' % school_name)
                         course_name = input('\033[34;1m课程名：\033[0m').strip()
+
 
                     elif choice == '2':
                         print('\033[32;1m招聘讲师\033[0m')
