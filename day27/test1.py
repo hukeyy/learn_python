caa5e74cd7e11febcee83dd339b51bc2c9190443
hukey <mickey.20@qq.com>
2018-09-02 16:27:12 +0800

    day27

---
+++ day27/test1.py
@@ -0,0 +1,20 @@
+class Course(object):
+    def __init__(self, name, price, time):
+        self.name = name
+        self.price = price
+        self.time = time
+
+
+class Grade(object):
+    def __init__(self, name, course):
+        self.name = name
+        self.course = course
+
+
+course = Course('python', 20000, 12)
+
+grade = Grade('三年二班', course)
+
+print(grade.course.name)
+print(grade.course.price)
+print(grade.course.time)
