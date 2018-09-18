4d77191b8f51e67ec26c7c787d9db935fccde7b5
hkey <mickey.20@qq.com>
2018-09-18 18:36:18 +0800

    day7

---
+++ day7/test2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: hkey
+
+# 第一步：
+f = open('text', 'r', encoding='utf-8') # (file, 文件操作模式, 文件编码)
+print(f.read())
+f.close()
+
+
+
+
+
+
+
+
+
+
