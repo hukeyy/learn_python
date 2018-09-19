bb5ab354787d683232edc75cf276281b77ed3328
hkey <mickey.20@qq.com>
2018-09-19 19:05:05 +0800

    day8

--- day8/test1.py
+++ day8/test1.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author: hkey
-data = 'xiaofei|123\n'
-with open('test.txt', 'a+') as f:
-    f.write(data)
+
+with open('test.txt', encoding='utf-8') as f, open('test1.txt', 'w', encoding='utf-8') as f1:
+    data = f.read()
+    print(data)
+    data = data.replace('hkey', 'xiaofei')
+    f1.write(data)
 
---
+++ day8/test2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: hkey
+s1 = '姓名：hkey'
+s1 = s1.replace('hkey', 'xiaofei')
+print(s1)
+
+
