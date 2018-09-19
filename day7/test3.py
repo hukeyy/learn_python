af244cdc5bde4753f69c0d7100fb5afea8061895
hkey <mickey.20@qq.com>
2018-09-19 22:33:38 +0800

    day7

---
+++ day7/test3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: hkey
+
+
+# def power(x, n):
+#     return x ** n
+#
+#
+# print(power(2, n = 4))
+
+# def calc(*args):
+#     n = 0
+#     for i in args:
+#         n += i
+#     return  n
+
+# def person(name, age, **kwargs):
+#     print('name:', name, 'age:', age, 'other:', kwargs)
+
+# def person(name, age, *, city, job):
+#     print(name, age, city, job)
+
+
+# def f1(a, b, c=3, *args, **kwargs):
+#     print('a=', a, 'b=', b, 'c=', c, 'arg=', args, 'kwargs=', kwargs)
+
+
+
+
+
+
+
+
+
