af10997bad94c4507450e68b68d1052cde22d24b
hkey <mickey.20@qq.com>
2018-09-20 22:22:05 +0800

    day10

---
+++ day10/test3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: hkey
+
+# dic = {'k1':'v1v1', 'k2':[11,22,33,44]}
+#
+# def func(**kwargs):
+#     for k, v in kwargs.items():
+#         kwargs[k] = v[:2]
+#     return kwargs
+
+
+# print(func(**dic))
+
+
+# def func(a, b):
+#     c = a if a> b else b
+#     return c
+#
+# print(func(1, 5))
+
+# c = lambda a, b: a if a > b else b
+# print(c(6, 5))
+
