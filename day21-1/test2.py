e804273b13711db02981d7d762306dce3099ffa0
hkey <mickey.20@qq.com>
2018-10-18 18:45:33 +0800

    day21

--- day21-1/test2.py
+++ day21-1/test2.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author: hkey
-import time
 
 
 def log(func):
     def wrapper(*args, **kwargs):
-        start_time = time.time()
         res = func(*args, **kwargs)
-        print('%s 执行时间: %s' % (func.__name__, time.time() - start_time))
         return res
 
     return wrapper
 
 
 @log
-def func():
-    time.sleep(1)
-    print('func():')
+def foo():
+    print('hello foo')
 
 
-func()
+foo()
