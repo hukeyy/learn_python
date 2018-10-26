346c240372c64f5cc5e98e0637027360ec985520
hkey <mickey.20@qq.com>
2018-10-26 18:15:00 +0800

    day24

--- day23-1/test-1.py
+++ day23-1/test-1.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author: hkey
-
 import os, pickle
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 __db_main = os.path.join(BASE_DIR, 'school_dict')
 __db_teacher = os.path.join(BASE_DIR, 'teacher_dict')
 
@@ -16,15 +15,15 @@ class School(object):
         self.addr = addr
 
     def cat_school(self):
-        print('\033[35;1m学校：【%s】\t地址：【%s】\033[0m' % (self.name, self.addr))
+        print('\033[32;1m学校【%s】\t地址【%s】\033[0m' % (self.name, self.addr))
 
-    def create_course(self, main_db, course, main_file):
+    def create_course(self, main_db, course, __db_main):
         main_db[self][course] = {}
-        file_oper(main_file, 'wb', main_db)
+        file_oper(__db_main, 'wb', main_db)
 
-    def create_teacher(self, main_db, course, teacher, main_file):
+    def hire_teahcer(self, main_db, course, teacher, __db_main):
         main_db[self][course] = {'teacher': teacher}
-        file_oper(main_file, 'wb', main_db)
+        file_oper(__db_main, 'wb', main_db)
 
     def create_grade(self, main_db, teacher_db, course, teacher, grade, main_file, teacher_file):
         main_db[self][course]['grade'] = grade
@@ -40,19 +39,18 @@ class Course(object):
         self.time = time
 
     def cat_course(self):
-        print('\033[35;1m课程：【%s】\t价格：【%s元】\t周期：【%s个月】\033[0m' % (self.name, self.price, self.time))
+        print('\033[32;1m课程【%s】\t价格【%s元】\t周期【%s个月】\033[0m' % (self.name, self.price, self.time))
 
 
 class Teacher(object):
-    def __init__(self, name, age, school, course, role='讲师'):
+    def __init__(self, name, age, school, course):
         self.name = name
         self.age = age
         self.school = school
         self.course = course
-        self.role = role
 
     def cat_teacher(self):
-        print('\033[35;1m课程：【%s】\t讲师：【%s】\033[0m' % (self.course, self.name))
+        print('\033[32;1m课程【%s】\t讲师【%s】\033[0m' % (self.course, self.name))
 
 
 class Grade(object):
@@ -63,11 +61,9 @@ class Grade(object):
         self.student = set([])
 
     def cat_grade(self):
-        print('\033[35;1m课程：【%s】\t讲师：【%s】\t班级：【%s】\033[0m' % (self.course,
-                                                              self.teacher, self.name))
+        print('\033[32;1m课程【%s】\t讲师【%s】\t班级【%s】\033[0m' % (self.course, self.teacher, self.name))
 
-    def add_student(self, teacher_db, teacher, student_name, teacher_file):
-        self.student.add(student_name)
+    def add_student(self, teacher_db, teacher, teacher_file):
         teacher_db[teacher] = {'grade': self}
         file_oper(teacher_file, 'wb', teacher_db)
 
@@ -92,13 +88,6 @@ def init_database():
     if not os.path.exists(__db_teacher):
         data = {}
         file_oper(__db_teacher, 'wb', data)
-
-
-def options(li):
-    for i, k in enumerate(li):
-        print(i+1, k)
-    choice = input('>>>').strip()
-    return choice
 
 
 def information(db, mode, *args):
@@ -131,60 +120,56 @@ def school_center():
     while Flag:
         main_db = file_oper(__db_main, 'rb')
         res_school = information(main_db, 'main')[0]
-        school_name = input('\033[34;1m选择学校名:\033[0m').strip()
+        school_name = input('\033[34;1m输入学校名:\033[0m')
         if school_name in res_school:
             school = res_school[school_name]
             while Flag:
                 choice = options(school_list)
                 if choice == '1':   # 创建课程
                     while True:
-                        print('\033[33;1m学校【%s】目前已经存在的课程信息：\033[0m' % school_name)
+                        print('\033[31;1m学校【%s】目前已有课程信息\033[0m' % school_name)
                         res_course = information(main_db[school], 'course')[0]
                         if not res_course:
-                            print('\033[31;1m目前没有课程信息.\033[0m')
-                        if_cont = input('\033[34;1m是否创建课程(y/b)：\033[0m').strip()
+                            print('\033[31;1m目前没有课程.\033[0m')
+                        if_cont = input('\033[34;1m是否创建课程(y/b):\033[0m').strip()
                         if if_cont == 'y':
-                            course_name = input('\033[34;1m输入课程名：\033[0m').strip()
+                            course_name = input('\033[34;1m输入课程名:\033[0m')
                             if course_name not in res_course:
-                                course_price = input('\033[34;1m输入课程价格：\033[0m').strip()
-                                course_time = input('\033[34;1m输入课程周期：\033[0m').strip()
+                                course_price = input('\033[34;1m输入课程价格:\033[0m')
+                                course_time = input('\033[34;1m输入课程周期:\033[0m')
                                 course = Course(course_name, course_price, course_time)
                                 school.create_course(main_db, course, __db_main)
                             else:
-                                print('\033[31;1m输入课程信息已存在，请重新输入.\033[0m')
+                                print('\033[31;1m输入课程名已存在，请重新输入.\033[0m')
                         elif if_cont == 'b':
                             break
                         else:
                             print('\033[31;1m输入错误，请重新输入.\033[0m')
                 elif choice == '2': # 招聘讲师
                     while True:
-                        print('\033[33;1m学校【%s】目前已经存在的讲师信息：\033[0m' % school_name)
+                        print('\033[31;1m学校【%s】目前已有讲师信息\033[0m' % school_name)
                         res_course = information(main_db[school], 'None')[0]
                         if res_course:
                             for i in res_course:
                                 k = res_course[i]
                                 res_teacher = information(main_db[school][k], 'teacher')[1]
                                 if not res_teacher:
-                                    print('\033[31;1m课程：【%s】\t讲师：【None】\033[0m' % k.name)
-
-                        if_cont = input('\033[34;1m是否招聘讲师(y/b)：\033[0m').strip()
+                                    print('\033[32;1m课程【%s】\t讲师【None】\033[0m' % k.name)
+                        if_cont = input('\033[34;1m是否招聘讲师(y/b):\033[0m').strip()
                         if if_cont == 'y':
-                            teacher_name = input('\033[34;1m输入讲师名：\033[0m').strip()
-                            teacher_age = input('\033[34;1m输入讲师年龄：\033[0m').strip()
-                            course_name = input('\033[34;1m输入讲师上的课程：\033[0m').strip()
+                            teacher_name = input('\033[34;1m输入讲师的名字:\033[0m').strip()
+                            teacher_age = input('\033[34;1m输入讲师的年龄:\033[0m').strip()
+                            course_name = input('\033[34;1m输入讲师要上课程的名字:\033[0m').strip()
                             if course_name in res_course:
                                 course = res_course[course_name]
                                 if teacher_name not in res_teacher:
                                     teacher = Teacher(teacher_name, teacher_age, school_name, course_name)
-                                    school.create_teacher(main_db, course, teacher, __db_main)
+                                    school.hire_teahcer(main_db, course, teacher, __db_main)
                                 else:
-                                    print('\033[31;1m讲师信息已存在.\033[0m')
-
+                                    print('\033[31;1m输入的讲师信息已存在，请重新输入.\033[0m')
 
                             else:
-                                print('\033[31;1m课程信息不存在，请重新输入.\033[0m')
-
-
+                                print('\033[31;1m输入的课程信息不存在，请重新输入.\033[0m')
                         elif if_cont == 'b':
                             break
                         else:
@@ -194,86 +179,82 @@ def school_center():
                     while True:
                         teacher_db = file_oper(__db_teacher, 'rb')
                         res_course = information(main_db[school], 'None')[0]
-                        print('\033[33;1m学校【%s】目前已经存在的班级信息：\033[0m' % school_name)
                         if res_course:
                             for i in res_course:
                                 k = res_course[i]
                                 res_grade = information(main_db[school][k], 'grade')[1]
-                                if not res_grade:
-                                    print('\033[31;1m目前没有班级信息.\033[0m')
-
-                        if_cont = input('\033[34;1m是否创建班级(y/b)：\033[0m').strip()
+                        if_cont = input('\033[34;1m是否创建班级(y/b):\033[0m').strip()
                         if if_cont == 'y':
-                            grade_name = input('\033[34;1m输入班级名：\033[0m').strip()
-                            course_name = input('\033[34;1m输入班级要上的课程:\033[0m').strip()
+                            grade_name = input('\033[34;1m输入要创建的班级名:\033[0m').strip()
+                            course_name = input('\033[34;1m输入班级要上的课程名:\033[0m').strip()
                             if course_name in res_course:
                                 course = res_course[course_name]
                                 if main_db[school][course]:
                                     teacher = main_db[school][course]['teacher']
                                     if grade_name not in res_grade:
                                         grade = Grade(grade_name, course_name, teacher.name)
-                                        school.create_grade(main_db, teacher_db, course, teacher,
-                                                            grade, __db_main, __db_teacher)
+                                        school.create_grade(main_db, teacher_db, course, teacher, grade,
+                                                            __db_main, __db_teacher)
                                     else:
-                                        print('\033[31;1m输入的班级信息已存在！\033[0m')
-
+                                        print('\033[31;1m班级信息已存在.\033[0m')
                                 else:
-                                    print('\033[31;1m输入的讲师不存在！\033[0m')
-
-
+                                    print('\033[31;1m没有讲师信息，请先创建讲师信息.\033[0m')
                             else:
-                                print('\033[31;1m输入的课程不存在！\033[0m')
+                                print('\033[31;1m输入课程信息不存在，请重新输入.\033[0m')
                         elif if_cont == 'b':
                             break
                         else:
                             print('\033[31;1m输入错误，请重新输入.\033[0m')
                 elif choice == '4': # 返回
                     Flag = False
+                else:
+                    print('\033[31;1m输入错误，请重新输入.\033[0m')
 
-
+        else:
+            print('\033[31;1m输入的学校名不存在，请重新输入.\033[0m')
 
 
 def teacher_center():
     print('\033[33;1m[teacher_center]\033[0m')
+    main_db = file_oper(__db_main, 'rb')
     teacher_db = file_oper(__db_teacher, 'rb')
-    res_teacher = information(teacher_db, 'None')[0]
-    print('res_teacher:', res_teacher)
     teacher_name = input('\033[34;1m输入讲师名:\033[0m').strip()
+    res_teacher = information(teacher_db, 'None')[0]
     if teacher_name in res_teacher:
         while True:
-            print('\033[32;1m欢迎进入【%s】讲师管理中心\033[0m' % teacher_name)
+            print("\33[32;1m欢迎进入讲师【%s】的管理中心\33[0m".center(40, "*") % teacher_name)
             choice = options(teacher_list)
             teacher = res_teacher[teacher_name]
-            if teacher_db[teacher].get('grade'):
-                grade = teacher_db[teacher]['grade']
-                if choice == '1':   # 查看班级
-                    print("\33[32;0m讲师【%s】的班级信息\33[0m".center(40, "-") % teacher.name)
-                    print('学校【%s】\t课程【%s】\t班级【%s】' % (teacher.school,
-                                                      grade.course, grade.name))
-                    any = input('\033[32;1m输入任意键退出当前:\033[0m')
-                elif choice == '2': # 查看班级学员列表
-                    print("\33[32;0m讲师【%s】的班级学员列表\33[0m".center(40, "-") % teacher.name)
-                    print('班级【%s】\t学员【%s】' % (grade.name, grade.student))
-                    any = input('\033[32;1m输入任意键退出当前:\033[0m')
-                elif choice == '3':
-                    break
-                else:
-                    print('\033[31;1m输入错误，请重新输入.\033[0m')
+            grade = teacher_db[teacher]['grade']
+            if choice == '1':   # 查看班级信息
+                print("\33[32;0m讲师【%s】的班级信息\33[0m".center(40, "-") % teacher.name)
+                print('\033[32;1m学校【%s】\t课程【%s】\t班级【%s】\033[0m' % (teacher.school, teacher.course,
+                                                                   grade.name))
+                any = input('\033[34;1m按任意键退出当前:\033[0m')
+            elif choice == '2': # 查看学员信息
+                print("\33[32;0m讲师【%s】的班级学员列表\33[0m".center(40, "-") % teacher.name)
+                print('\033[32;1m班级【%s】\t学员【%s】\033[0m' % (grade.name, grade.student))
+                any = input('\033[34;1m按任意键退出当前:\033[0m')
+
+            elif choice == '3': # 返回
+                break
+
+
 
 def student_center():
     print('\033[33;1m[student_center]\033[0m')
     while True:
+        main_db = file_oper(__db_main, 'rb')
+        teacher_db = file_oper(__db_teacher, 'rb')
         choice = options(student_list)
-        if choice == '1':   # 学员注册
-            main_db = file_oper(__db_main, 'rb')
-            teacher_db = file_oper(__db_teacher, 'rb')
-            student_name = input('\033[34;1m输入学生名：\033[0m').strip()
+        if choice == '1':
+            student_name = input('\033[34;1m输入注册学员姓名:\033[0m').strip()
             res_school = information(main_db, 'main')[0]
-            school_name = input('\033[34;1m选择学校名：\033[0m').strip()
+            school_name = input('\033[34;1m输入学校名:\033[0m').strip()
             if school_name in res_school:
                 school = res_school[school_name]
                 res_course = information(main_db[school], 'course')[0]
-                course_name = input('\033[34;1m选择课程名：\033[0m').strip()
+                course_name = input('\033[34;1m输入课程名:\033[0m').strip()
                 if course_name in res_course:
                     course = res_course[course_name]
                     if main_db[school][course].get('grade'):
@@ -281,24 +262,33 @@ def student_center():
                             if i.course == course.name:
                                 teacher = i
                                 grade = teacher_db[teacher]['grade']
-
-                        print('\033[34;1m课程【%s】的费用为【%s】\033[0m' %(course.name, course.price))
+                        print('\033[32;1m课程【%s】的费用为【%s】\033[0m' % (course.name, course.price))
                         if_pay = input('\033[34;1m是否支付当前费用(y):\033[0m').strip()
                         if if_pay == 'y':
-                            grade.add_student(teacher_db, teacher, student_name, __db_teacher)
-                            print('\033[32;1m选课成功.\033[0m')
-                            any = input('\033[32;1m输入任意键退出当前:\033[0m')
+                            grade.student.add(student_name)
+                            grade.add_student(teacher_db, teacher, __db_teacher)
+                            print('\033[32;1m支付成功.\033[0m')
+                            any = input('\033[34;1m按任意键退出当前:\033[0m')
                     else:
-                        print('\032[33;1m讲师信息不存在.\033[0m')
+                        print('\033[31;1m讲师不存在，请先招聘讲师\033[0m')
                 else:
-                    print('\032[33;1m输入的学校名不存在。\033[0m')
-            else:
-                print('\032[33;1m输入的学校名不存在。\033[0m')
+                    print('\033[31;1m输入的课程名不存在，请重新输入.\033[0m')
 
-        elif choice == '2': # 返回
+
+            else:
+                print('\033[31;1m输入的学校名不存在，请重新输入.\033[0m')
+
+        elif choice == '2':
             break
-        else:
-            print('\033[31;1m输入错误，请重新输入.\033[0m')
+
+
+
+def options(li):
+    for i, k in enumerate(li):
+        print(i+1, k)
+    choice = input('>>>').strip()
+    return choice
+
 
 def start():
     while True:
@@ -317,11 +307,13 @@ def start():
 
 if __name__ == '__main__':
     init_database()
-    main_list = ['学校中心', '讲师中心', '学生中心', '退出']
+    main_list = ['学校中心', '教师中心', '学生中心', '退出']
     school_list = ['创建课程', '招聘讲师', '创建班级', '返回']
     student_list = ['学员注册', '返回']
-    teacher_list = ['查看班级', '查看班级学员列表', '返回']
+    teacher_list = ['查看班级信息', '查看班级学员列表', '返回']
     start()
+
+
 
 
 
