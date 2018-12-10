fe2a476aa3ac061eb8c9fd09d29f5a65dea9ef48
hkey <mickey.20@qq.com>
2018-12-10 18:11:24 +0800

    day31 ftp

--- socket_test/client/client.py
+++ socket_test/client/client.py
@@ -23,22 +23,67 @@
 
 
 
-import socket, os, json
+# import socket, os, json
+#
+# dic = {'filename': 'test.txt', 'filesize':None}
+# dic['filesize'] = os.path.getsize(dic['filename'])
+# dic_json = json.dumps(dic)
+# sk_client = socket.socket()
+# sk_client.connect(('127.0.0.1', 8080))
+# sk_client.send(dic_json.encode())
+# res_code = sk_client.recv(1024).decode()
+# with open(dic['filename'], 'rb') as f:
+#     while True:
+#         data = f.read(1024)
+#         if not data:
+#             break
+#         sk_client.send(data)
+# sk_client.close()
 
-dic = {'filename': 'test.txt', 'filesize':None}
-dic['filesize'] = os.path.getsize(dic['filename'])
-dic_json = json.dumps(dic)
+import socket, hashlib
+md5 = hashlib.md5()
 sk_client = socket.socket()
 sk_client.connect(('127.0.0.1', 8080))
-sk_client.send(dic_json.encode())
-res_code = sk_client.recv(1024).decode()
-with open(dic['filename'], 'rb') as f:
-    while True:
-        data = f.read(1024)
-        if not data:
-            break
-        sk_client.send(data)
+cmd = input('>>>').strip()
+sk_client.send(cmd.encode())
+f_info = sk_client.recv(1024).decode()
+sk_client.send(b'200')
+f_name, f_size = f_info.split('|')
+f_size = int(f_size)
+
+revice_size = 0
+with open(f_name, 'wb') as f:
+    while revice_size != f_size:
+        data = sk_client.recv(1024)
+        md5.update(data)
+        revice_size += len(data)
+        print(revice_size, f_size)
+        f.write(data)
+sk_client.send(b'ok')
+sf_md5 = sk_client.recv(1024)
+if sf_md5.decode() == md5.hexdigest():
+    print(sf_md5.decode(), md5.hexdigest())
+    print('文件传输完成。')
 sk_client.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
